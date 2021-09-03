e8c1eb854698f78134bfafca0c51637154993981
pritindra-bhowmick <86006254+pritindra-bhowmick@users.noreply.github.com>
2021-09-03 23:54:04 +0530

    Update generate_voltage.py

--- generate_voltage.py
+++ generate_voltage.py
@@ -1,3 +1,10 @@
+# Work done by Pritindra Bhowmick, EP-DT-DT, CERN during Summer Studentship 2021 at CERN.
+# Project name: High precision calibration of 3D hall probes for magnet mapping apparatuses.
+# Find me at pritindra2001@gmail.com, pritindra.bhowmick@cern.ch
+# Find the project report at CERN CDS url coming soon
+# History : Version 1: 04-09-2021
+
+
 # ------------------------------------------------------------------------------
 # 1 : importing packages
 # ------------------------------------------------------------------------------
